c12c58539766ffabc0f62fc5d5ce2a6d8f54f62a
Itay Finci <Itay@Finci.org>
2020-02-11 16:41:34 +0200

    Improved file in 1. added option for choosing the file that will be loaded with "-f" argument followed by the file path. 2. added default file paht "stocks.dt" that will load this file from the directory that the user is currently in. 3. added error messege when file was not found. 4. when file is not found then the app will close with the user click on ok. 5. added to the gitignore stocks.dt so private stock information wont be uploaded.
    Signed-off-by: Itay Finci <Itay@Finci.org>

--- src/finci/org/StockValue/Main.java
+++ src/finci/org/StockValue/Main.java
@@ -7,21 +7,24 @@ import java.util.*;
 
 public class Main {
     public static final int CLOCK_CYCLE_MILLIS = 300000;
+    private static String fileIn;
     public static void main(String[] args) {
         LinkedList<Stock> head = new LinkedList<>();
         //make the view
-        JFrame frame = new JFrame("Stocks");
-        ShowInfo si = new ShowInfo(frame);
-        JScrollPane sp = new JScrollPane(si.getTbInfo());
-        frame.add(sp);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        JFrame frame = new JFrame("Stocks");;
+        ShowInfo si = makeView(frame);
+        //initialize the file name
+        fileIn = getFileName(args);
         //initialize the list
         try {
             initialize(head,si);
         }catch (FileNotFoundException f){
-            System.out.println("check file exsistance");
+            //show error message
+            JOptionPane.showMessageDialog(frame, "file not found.\n use \"-f\" argument to point on file.\n" +
+                                            "use stocks.dt file as default.",
+                                            "file not found", JOptionPane.ERROR_MESSAGE);
+            //close the program
+            System.exit(0);
         }
         //add a clock
         new Thread(()-> {
@@ -38,6 +41,31 @@ public class Main {
         }).start();
     }
 
+    static ShowInfo makeView(JFrame frame){
+        ShowInfo si = new ShowInfo(frame);
+        JScrollPane sp = new JScrollPane(si.getTbInfo());
+        frame.add(sp);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        return si;
+    }
+
+    /**
+     * if user enter the argument -f, then the next argument is the file name.
+     * default value is stocks.dt file.
+     * @param args the program arguments.
+     * @return the file path
+     */
+    static String getFileName(String[] args){
+        for (int i = 0; i < args.length-1; i++) {
+            if(args[i].equals("-f")){
+                return args[i+1];
+            }
+        }
+        return "./stocks.dt";
+    }
+
     static void update(LinkedList<Stock> stocks, ShowInfo si){
         LinkedList<Thread> threads = new LinkedList<>();
         for(Stock s: stocks){
@@ -50,7 +78,7 @@ public class Main {
 
     static void initialize(LinkedList<Stock> stocks, ShowInfo si) throws FileNotFoundException{
         //initialize from file
-        File file = new File("stocks.dt");
+        File file = new File(fileIn);
         Scanner scan = new Scanner(file);
         LinkedList<Thread> threads = new LinkedList<>();
         while(scan.hasNext()){
